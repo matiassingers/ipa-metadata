57ca9bc538e35462eea741059c180e355c69c6d8
Matias Singers <mail@matiassingers.com>
2014-11-20 22:14:42 +0800

    Create inital logic for reading, extracting and converting the .plist files

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(){
+var fs = require('fs');
+var exec = require('child_process').exec;
+var plist = require('plist');
+var unzip = require('unzip');
 
+var rimraf = require('rimraf');
+var tmp = require('temporary');
+var glob = require("glob");
+
+var output = new tmp.Dir();
+
+module.exports = function (file, callback){
+  fs.createReadStream(file)
+    .pipe(unzip.Extract({ path: output.path }))
+    .on('close', function(){
+      var path = glob.sync(output.path + '/Payload/*/')[0];
+
+      var metadata = plist.parse(fs.readFileSync(path + 'Info.plist', 'utf8'));
+
+      exec('security cms -D -i embedded.mobileprovision > Provisioning.plist', { cwd: path }, function() {
+        var provisioning = plist.parse(fs.readFileSync(path + 'Provisioning.plist', 'utf8'));
+        delete provisioning.DeveloperCertificates;
+
+        rimraf.sync(output.path);
+
+        return callback({
+          metadata: metadata,
+          provisioning: provisioning
+        });
+      });
+    });
 };
