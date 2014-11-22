e1fedbd6041b3897d980509260780ff355d78eac
Matias Singers <mail@matiassingers.com>
2014-11-22 18:13:10 +0800

    Add initial working simple unit test spec

--- test.js
+++ test.js
@@ -3,8 +3,27 @@
 var assert = require('assert');
 var ipaMetadataParser = require('./');
 
-describe('initial', function(){
-  it('fail', function() {
-    assert(false);
+describe('should parse .ipa file', function(){
+  var error;
+  var output;
+  beforeEach(function(done) {
+    ipaMetadataParser('tests/fixtures/testApp.ipa', function(err, data) {
+      error = err;
+      output = data;
+
+      done();
+    });
+  });
+
+  it('error should not be set', function() {
+    assert.equal(error, void 0);
+  });
+
+  it('should contain the correct data', function() {
+    assert.equal(output.metadata['CFBundleDisplayName'], 'Test App');
+    assert.equal(output.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+    assert.equal(output.metadata['CFBundleName'], 'Test App');
+    assert.equal(output.metadata['CFBundleShortVersionString'], '1.0.0');
+    assert.equal(output.metadata['CFBundleVersion'], '1337');
   });
 });
