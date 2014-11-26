6d49cbbdb8e49209e4728850ec0a3c1406adb854
Matias Singers <mail@matiassingers.com>
2014-11-26 14:14:30 +0800

    Switch to `decompress-zip` dependency instead of `unzip`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 var plist = require('plist');
-var unzip = require('unzip');
+var decompress = require('decompress-zip');
 var which = require('which');
 
 var rimraf = require('rimraf');
@@ -17,35 +17,39 @@ var provisionFilename = 'Provisioning.plist';
 module.exports = function (file, callback){
   var data = {};
 
-  fs.createReadStream(file)
-    .pipe(unzip.Extract({ path: output.path }))
-    .on('close', function(){
-      var path = glob.sync(output.path + '/Payload/*/')[0];
+  var unzipper = new decompress(file);
+  unzipper.extract({
+    path: output.path
+  });
 
-      data.metadata = plist.parse(fs.readFileSync(path + 'Info.plist', 'utf8'));
+  unzipper.on('error', cleanUp);
+  unzipper.on('extract', function() {
+    var path = glob.sync(output.path + '/Payload/*/')[0];
 
-      if(!fs.existsSync(path + 'embedded.mobileprovision') || !which.sync('security')){
+    data.metadata = plist.parse(fs.readFileSync(path + 'Info.plist', 'utf8'));
+
+    if(!fs.existsSync(path + 'embedded.mobileprovision') || !which.sync('security')){
+      return cleanUp();
+    }
+    exec('security cms -D -i embedded.mobileprovision > ' + provisionFilename, { cwd: path }, function(error) {
+      if(error){
+        cleanUp(error);
+      }
+
+      data.provisioning = plist.parse(fs.readFileSync(path + provisionFilename, 'utf8'));
+      delete data.provisioning.DeveloperCertificates;
+
+      if(!which.sync('codesign')){
         return cleanUp();
       }
-      exec('security cms -D -i embedded.mobileprovision > ' + provisionFilename, { cwd: path }, function(error) {
-        if(error){
-          cleanUp(error);
-        }
 
-        data.provisioning = plist.parse(fs.readFileSync(path + provisionFilename, 'utf8'));
-        delete data.provisioning.DeveloperCertificates;
+      exec('codesign -d --entitlements :- ' + path, function(error, output) {
+        data.entitlements = plist.parse(output);
 
-        if(!which.sync('codesign')){
-          return cleanUp();
-        }
-
-        exec('codesign -d --entitlements :- ' + path, function(error, output) {
-          data.entitlements = plist.parse(output);
-
-          return cleanUp();
-        });
+        return cleanUp();
       });
     });
+  });
 
   function cleanUp(error){
     rimraf.sync(output.path);
