c6823d406085d256a1b3a5578f8b9f6e8e650e82
Matias Singers <mail@matiassingers.com>
2015-01-22 08:18:43 +0800

    Add test case + fixture with a binary plist file

--- test.js
+++ test.js
@@ -18,3 +18,19 @@ describe('should parse .ipa file', function(){
     });
   });
 });
+
+describe('should parse .ipa files with binary .plist files', function(){
+  it('should contain the correct data', function(done) {
+    ipaMetadataParser('tests/fixtures/testApp-binaryPlist.ipa', function(err, data) {
+      assert.equal(err, void 0);
+
+      assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
+      assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+      assert.equal(data.metadata['CFBundleName'], 'Test App');
+      assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
+      assert.equal(data.metadata['CFBundleVersion'], '1337');
+
+      done();
+    });
+  });
+});
