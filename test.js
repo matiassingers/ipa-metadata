93d1e8d6935ab7fec250ae9a74a806ab81552473
Matias Singers <mail@matiassingers.com>
2015-06-09 20:41:30 +0800

    Mock provisioning and entitlements in unit tests - 100% test coverage 🎉

--- test.js
+++ test.js
@@ -1,38 +1,134 @@
 'use strict';
 
 var assert = require('assert');
-var ipaMetadataParser = require('./');
+var proxyquire =  require('proxyquire');
 
-describe('should parse .ipa file', function(){
-  it('should contain the correct data', function(done) {
-    ipaMetadataParser('tests/fixtures/testApp.ipa', function(err, data) {
-      assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
-      assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
-      assert.equal(data.metadata['CFBundleName'], 'Test App');
-      assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
-      assert.equal(data.metadata['CFBundleVersion'], '1337');
 
-      // Property is only set on OS X
-      assert.equal(data.entitlements, void 0);
 
-      done();
+var ipaMetadataParser = proxyquire('./', {
+  provisioning: function provisioningStub(file, callback) {
+    callback(null, {
+      key: 'lol',
+      DeveloperCertificates: 'whatever'
     });
-  });
+  },
+  entitlements: function entitlementsStub(file, callback) {
+    callback(null, {
+      anotherKey: 'test'
+    });
+  }
 });
 
-describe('should parse .ipa files with binary .plist files', function(){
-  it('should contain the correct data', function(done) {
-    ipaMetadataParser('tests/fixtures/testApp-binaryPlist.ipa', function(err, data) {
-      assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
-      assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
-      assert.equal(data.metadata['CFBundleName'], 'Test App');
-      assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
-      assert.equal(data.metadata['CFBundleVersion'], '1337');
+describe('ipa-metadata', function() {
+  describe('should not contain entitlements on fake platform', function() {
+    beforeEach(function() {
+      this.originalPlatform = process.platform;
+      Object.defineProperty(process, 'platform', {
+        value: 'MockOS'
+      });
+    });
 
-      // Property is only set on OS X
-      assert.equal(data.entitlements, void 0);
+    describe('should parse .ipa file', function() {
+      it('should contain the correct data', function(done) {
+        ipaMetadataParser('tests/fixtures/testApp.ipa', function(err, data) {
+          assert.equal(err, void 0);
 
-      done();
+          assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
+          assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+          assert.equal(data.metadata['CFBundleName'], 'Test App');
+          assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
+          assert.equal(data.metadata['CFBundleVersion'], '1337');
+
+          assert.deepEqual(data.provisioning, {key: 'lol'});
+          assert.equal(data.entitlements, void 0);
+
+          done();
+        });
+      });
+    });
+
+    describe('should parse .ipa files with binary .plist files', function() {
+      it('should contain the correct data', function(done) {
+        ipaMetadataParser('tests/fixtures/testApp-binaryPlist.ipa', function(err, data) {
+          assert.equal(err, void 0);
+
+          assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
+          assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+          assert.equal(data.metadata['CFBundleName'], 'Test App');
+          assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
+          assert.equal(data.metadata['CFBundleVersion'], '1337');
+
+          assert.deepEqual(data.provisioning, {key: 'lol'});
+          assert.equal(data.entitlements, void 0);
+
+          done();
+        });
+      });
+    });
+
+    afterEach(function() {
+      Object.defineProperty(process, 'platform', {
+        value: this.originalPlatform
+      });
+    });
+  });
+
+  describe('should contain entitlements on OS X platform', function() {
+    beforeEach(function() {
+      this.originalPlatform = process.platform;
+      Object.defineProperty(process, 'platform', {
+        value: 'darwin'
+      });
+    });
+
+    it('should contain the correct data', function(done) {
+      ipaMetadataParser('tests/fixtures/testApp-binaryPlist.ipa', function(err, data) {
+        assert.equal(err, void 0);
+
+        assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
+        assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+        assert.equal(data.metadata['CFBundleName'], 'Test App');
+        assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
+        assert.equal(data.metadata['CFBundleVersion'], '1337');
+
+        assert.deepEqual(data.provisioning, {key: 'lol'});
+        assert.deepEqual(data.entitlements, {anotherKey: 'test'});
+
+        done();
+      });
+    });
+
+    afterEach(function() {
+      Object.defineProperty(process, 'platform', {
+        value: this.originalPlatform
+      });
+    });
+  });
+
+  describe('should handle parsing errors', function() {
+    beforeEach(function() {
+      ipaMetadataParser = proxyquire('./', {
+        provisioning: function provisioningStub(file, callback) {
+          callback('fake error');
+        }
+      });
+    });
+
+    it('should contain the correct data', function(done) {
+      ipaMetadataParser('tests/fixtures/testApp-binaryPlist.ipa', function(err, data) {
+        assert.equal(err, 'fake error');
+
+        assert.equal(data.metadata['CFBundleDisplayName'], 'Test App');
+        assert.equal(data.metadata['CFBundleIdentifier'], 'io.mts.testapp');
+        assert.equal(data.metadata['CFBundleName'], 'Test App');
+        assert.equal(data.metadata['CFBundleShortVersionString'], '1.0.0');
+        assert.equal(data.metadata['CFBundleVersion'], '1337');
+
+        assert.equal(data.provisioning, void 0);
+        assert.equal(data.entitlements, void 0);
+
+        done();
+      });
     });
   });
 });
